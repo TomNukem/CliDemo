2e5da7d03eedc104631180a609753af617428ecf
--global <--global>
2024-07-20 15:42:32 +0200

    Initial commit

---
+++ DemoApp/Program.cs
@@ -0,0 +1,3 @@
+﻿// dotnet publish -p:PublishSingleFile=true -r linux-x64 --self-contained false
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
